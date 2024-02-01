2d2a2f8cdb52ec38ff9cb52cade8244b1fa9e745
Aleksandr <semenovichas1@yandex.ru>
2024-02-01 15:57:47 +0400

    Add files via upload

---
+++ ToyStore.java
@@ -0,0 +1,34 @@
+
+public class ToyStore {
+
+    public void addToy(Toy toy1) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addToy'");
+    }
+
+    public void showAvailableToys() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'showAvailableToys'");
+    }
+
+    public void updateToyWeight(int i, int j) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateToyWeight'");
+    }
+
+    public void playToys() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'playToys'");
+    }
+
+    public void getPrizeToy() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPrizeToy'");
+    }
+
+    public void addToy(int i, String string, int j, int k) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addToy'");
+    }
+
+}
